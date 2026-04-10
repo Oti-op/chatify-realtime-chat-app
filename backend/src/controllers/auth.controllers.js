214a76ac9f5e98c5b814cc042128b8c7807f6fad
Oti Oseji <Otiteos49@gmail.com>
2026-04-10 21:44:16 +0100

    signup refactoring

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -8,10 +8,11 @@ import { ENV } from "../lib/env.js";
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
 
-    try {
-        if (!fullName || !email || !password) {
+     if (!fullName || !email || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
+
+    try {
 
         if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters" });
