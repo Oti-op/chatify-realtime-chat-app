f9cfb30600c624a99855dc6eb52b1ac638da8c4f
Oti Oseji <Otiteos49@gmail.com>
2026-04-26 23:48:50 +0100

    .

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,60 @@
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const getAllContacts = async (req, res) => {
+    try {
+        const loggedInUser = req.user._id;
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUser } }).select("-password");
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.error("Error fetching contacts:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getMessagesByUserId = async (req, res) => {
+    try {
+        const myId = req.user._id;
+        const { id: userToChatId } = req.params;
+
+        const message = await Message.find({
+            $or: [
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:myId}
+            ]
+        });
+
+        res.status(200).json(message);
+    } catch (error) {
+        console.error("Error fetching messages:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            text,
+            senderId,
+            receiverId,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+        
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.error("Error sending message:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
